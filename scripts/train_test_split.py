585bb109bc8911a60d32dcadba88a3ac2b531b3c
Ian Heung （香凱仁） <98202025+iaheung@users.noreply.github.com>
2024-08-21 10:30:10 -0400

    Add files via upload

---
+++ scripts/train_test_split.py
@@ -0,0 +1,36 @@
+"""
+Performs train test split on player_data.csv
+
+Authors:
+- Ian Heung
+- Chinedu Okeke
+
+Date Started: April 28th, 2024
+Last Edited: April 28th, 2024
+"""
+
+import os
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+def main():
+    df = pd.read_csv('processed_data/player_data.csv')
+
+    save_dir = "training_data"
+
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+
+    train_df, test_df = train_test_split(df, test_size=0.3)
+
+    # Drops extra column to not be used for training
+    train_df.drop(columns=["Unnamed: 0"], inplace=True)
+    test_df.drop(columns=["Unnamed: 0"], inplace=True)
+
+    train_df.to_csv(os.path.join(save_dir, "train.csv"))
+    test_df.to_csv(os.path.join(save_dir, "test.csv"))
+
+    print(f"Data saved to {save_dir}")
+
+if __name__ == "__main__":
+    main()
